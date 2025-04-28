01c87e6c8b7f94d04610aa078353048e002f2f6d
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-04-28 23:43:48 +0300

    feat: added implementation of dictionary entity

---
+++ src/main/java/ru/vstu/adddict/entity/Dictionary.java
@@ -0,0 +1,97 @@
+package ru.vstu.adddict.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+@Table(name="dictionaries")
+public class Dictionary extends BaseDictionary{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "is_public", nullable = false)
+    private boolean isPublic;
+
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "author_id", nullable = false)
+    private Long authorId;
+
+    @Override
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public void setPublic(boolean isPublic) {
+        this.isPublic = isPublic;
+    }
+
+    @Override
+    public boolean isPublic() {
+        return isPublic;
+    }
+
+    @Override
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    @Override
+    public void setAuthorId(Long authorId) {
+        this.authorId = authorId;
+    }
+
+    @Override
+    public Long getAuthorId() {
+        return authorId;
+    }
+}
