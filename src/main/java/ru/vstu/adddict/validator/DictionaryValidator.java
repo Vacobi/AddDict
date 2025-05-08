c6165e145727a04fa14174076309ce5e334b91f1
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-05-08 22:05:15 +0300

    feat: added dictionary validator

---
+++ src/main/java/ru/vstu/adddict/validator/DictionaryValidator.java
@@ -0,0 +1,62 @@
+package ru.vstu.adddict.validator;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+import ru.vstu.adddict.dto.CreateDictionaryRequestDto;
+import ru.vstu.adddict.exception.ClientExceptionName;
+import ru.vstu.adddict.exception.GroupValidationException;
+import ru.vstu.adddict.exception.ValidationException;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class DictionaryValidator {
+
+    private final int nameLength;
+    private final int descriptionLength;
+
+    public DictionaryValidator(
+            @Qualifier("dictionaryNameLength") int nameLength,
+            @Qualifier("dictionaryNameLength") int descriptionLength
+    ) {
+        this.nameLength = nameLength;
+        this.descriptionLength = descriptionLength;
+    }
+
+    public Optional<GroupValidationException> validateCreateDictionaryRequest(CreateDictionaryRequestDto createDictionaryRequestDto) {
+        List<ValidationException> exceptions = new LinkedList<>();
+
+        exceptions.addAll(validateName(createDictionaryRequestDto.getName()));
+        exceptions.addAll(validateDescription(createDictionaryRequestDto.getDescription()));
+
+        return exceptions.isEmpty() ? Optional.empty() : Optional.of(new GroupValidationException(exceptions));
+    }
+
+    public List<ValidationException> validateName(String name) {
+        List<ValidationException> exceptions = new LinkedList<>();
+
+        if (name.length() > nameLength) {
+            String exceptionDescription =
+                    "Max length of dictionary name is " + nameLength + " symbols.";
+
+            exceptions.add(new ValidationException(exceptionDescription, ClientExceptionName.INVALID_DICTIONARY_NAME));
+        }
+
+        return exceptions;
+    }
+
+    public List<ValidationException> validateDescription(String description) {
+        List<ValidationException> exceptions = new LinkedList<>();
+
+        if (description.length() > descriptionLength) {
+            String exceptionDescription =
+                    "Max length of dictionary description is " + descriptionLength + " symbols.";
+
+            exceptions.add(new ValidationException(exceptionDescription, ClientExceptionName.INVALID_DICTIONARY_DESCRIPTION));
+        }
+
+        return exceptions;
+    }
+}
