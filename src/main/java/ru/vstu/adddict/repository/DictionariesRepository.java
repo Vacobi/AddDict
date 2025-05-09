728f77c6446779eef11ba945de2fabce475d1b36
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-05-09 22:38:31 +0300

    feat: added method for update with pessimistic lock in DictionariesRepository

--- src/main/java/ru/vstu/adddict/repository/DictionariesRepository.java
+++ src/main/java/ru/vstu/adddict/repository/DictionariesRepository.java
@@ -1,7 +1,33 @@
 package ru.vstu.adddict.repository;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import ru.vstu.adddict.entity.Dictionary;
 
+import java.util.NoSuchElementException;
+import java.util.function.UnaryOperator;
+
 public interface DictionariesRepository extends JpaRepository<Dictionary, Long> {
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    @Lock(LockModeType.PESSIMISTIC_READ)
+    default Dictionary updateWithLock(Long id, UnaryOperator<Dictionary> modifier) {
+        return update(id, true, modifier);
+    }
+
+    private Dictionary update(Long id, boolean updateIfUnmodified, UnaryOperator<Dictionary> modifier) {
+
+        Dictionary dictionary = findById(id).orElseThrow(NoSuchElementException::new);
+
+        Dictionary updated = modifier.apply(dictionary);
+
+        if (updateIfUnmodified || !dictionary.equals(updated)) {
+            return save(updated);
+        }
+
+        return updated;
+    }
 }
