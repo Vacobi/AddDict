cec01e9c8194cad9cd6db4ebf60de5043f53740d
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-05-09 22:13:44 +0300

    feat: added client exception names to CreateTranslationRequestDto validation

--- src/main/java/ru/vstu/adddict/exception/ClientExceptionName.java
+++ src/main/java/ru/vstu/adddict/exception/ClientExceptionName.java
@@ -6,6 +6,10 @@ import lombok.Getter;
 public enum ClientExceptionName {
     VALIDATION_EXCEPTION(800),
     GROUP_VALIDATION_EXCEPTION(801);
+    GROUP_VALIDATION_EXCEPTION(801),
+
+    INVALID_DICTIONARY_NAME(300),
+    INVALID_DICTIONARY_DESCRIPTION(301);
 
     private final int apiErrorCode;
 
