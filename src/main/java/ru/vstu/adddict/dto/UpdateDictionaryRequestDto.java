3cb364fa951a578307de2502323b80444283ef83
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-05-09 22:31:13 +0300

    feat: added UpdateDictionaryRequestDto

---
+++ src/main/java/ru/vstu/adddict/dto/UpdateDictionaryRequestDto.java
@@ -0,0 +1,17 @@
+package ru.vstu.adddict.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class UpdateDictionaryRequestDto {
+    private String name;
+    private String description;
+    private Boolean isPublic;
+    private Long requestSenderId;
+}
