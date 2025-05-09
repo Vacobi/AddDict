ef4b231307a79897938eff7189d409a623b10238
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-05-09 22:00:09 +0300

    refactor: added not null constraint to created_ad column of Dictionary entity

--- src/main/java/ru/vstu/adddict/entity/Dictionary.java
+++ src/main/java/ru/vstu/adddict/entity/Dictionary.java
@@ -29,7 +29,7 @@ public class Dictionary extends BaseDictionary{
     private Boolean isPublic;
 
     @ColumnDefault("CURRENT_TIMESTAMP")
-    @Column(name = "created_at")
+    @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
 
     @Column(name = "author_id", nullable = false)
