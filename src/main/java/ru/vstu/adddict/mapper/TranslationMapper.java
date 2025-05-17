00ed477c53f93abfbedb57d70c8998cc197cafe2
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-05-17 16:25:03 +0300

    feat: added TranslationMapper

---
+++ src/main/java/ru/vstu/adddict/mapper/TranslationMapper.java
@@ -0,0 +1,21 @@
+package ru.vstu.adddict.mapper;
+
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+import ru.vstu.adddict.dto.TranslationDto;
+import ru.vstu.adddict.entity.Translation;
+
+import java.time.LocalDateTime;
+
+@Mapper(
+        componentModel = "spring",
+        injectionStrategy = InjectionStrategy.FIELD,
+        unmappedSourcePolicy = ReportingPolicy.IGNORE,
+        imports = {LocalDateTime.class}
+)
+public interface TranslationMapper {
+
+    TranslationDto toTranslationDto(Translation savedTranslation);
+}
+
