ea449fee92c29d65ca0d21a2650a13a72800ad5e
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-05-17 17:10:38 +0300

    feat: added TranslationController

---
+++ src/main/java/ru/vstu/adddict/controller/TranslationController.java
@@ -0,0 +1,14 @@
+package ru.vstu.adddict.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RestController
+@RequestMapping("api/v1/translations")
+@RequiredArgsConstructor
+public class TranslationController {
+
+}
