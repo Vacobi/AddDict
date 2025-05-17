0f6d6904ef13e5d6dd398509726a0c724a6e554b
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-05-17 21:59:34 +0300

    feat: added page mapper method

--- src/main/java/ru/vstu/adddict/mapper/TranslationMapper.java
+++ src/main/java/ru/vstu/adddict/mapper/TranslationMapper.java
@@ -5,11 +5,14 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 import ru.vstu.adddict.dto.CreateTranslationRequestDto;
+import ru.vstu.adddict.dto.PageResponseDto;
 import ru.vstu.adddict.dto.TranslationDto;
 import ru.vstu.adddict.dto.TranslationResponseDto;
 import ru.vstu.adddict.entity.Translation;
 
 import java.time.LocalDateTime;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Mapper(
         componentModel = "spring",
@@ -25,5 +28,19 @@ public interface TranslationMapper {
     Translation toTranslation(CreateTranslationRequestDto dto);
 
     TranslationResponseDto toTranslationResponseDto(TranslationDto dto);
+
+    default <S, D> PageResponseDto<D> fromPageResponseDto(PageResponseDto<S> page, Function<S, D> map) {
+        return PageResponseDto.<D>builder()
+                .page(page.getPage())
+                .pageSize(page.getPageSize())
+                .totalPages(page.getTotalPages())
+                .totalElements(page.getTotalElements())
+                .content(
+                        page.getContent().stream()
+                                .map(map)
+                                .collect(Collectors.toList())
+                )
+                .build();
+    }
 }
 
