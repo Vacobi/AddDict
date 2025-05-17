7749760f2c24ada61e49cd2ded43caefb91e2cce
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-05-17 21:56:26 +0300

    feat: added PageResponseDto

---
+++ src/main/java/ru/vstu/adddict/dto/PageResponseDto.java
@@ -0,0 +1,20 @@
+package ru.vstu.adddict.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class PageResponseDto<D> {
+    private List<D> content;
+    private int page;
+    private long pageSize;
+    private long totalPages;
+    private long totalElements;
+}
