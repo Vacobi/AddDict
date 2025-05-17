bba41c5c728ffbf46d538493e159b31285eb0a98
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-05-17 20:41:36 +0300

    feat: added TranslationResponseDto

---
+++ src/main/java/ru/vstu/adddict/dto/TranslationResponseDto.java
@@ -0,0 +1,17 @@
+package ru.vstu.adddict.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class TranslationResponseDto {
+    private Long id;
+    private String originText;
+    private String translationText;
+    private Long dictionaryId;
+}
