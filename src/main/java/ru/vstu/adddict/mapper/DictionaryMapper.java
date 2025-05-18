35983a243611794ef9d4d805ef74a475b62f79fe
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-05-18 04:38:56 +0300

    feat: added page mapper in DictionaryMapper

--- src/main/java/ru/vstu/adddict/mapper/DictionaryMapper.java
+++ src/main/java/ru/vstu/adddict/mapper/DictionaryMapper.java
@@ -4,6 +4,7 @@ import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
+import ru.vstu.adddict.dto.PageResponseDto;
 import ru.vstu.adddict.dto.dictionary.CreateDictionaryRequestDto;
 import ru.vstu.adddict.dto.dictionary.DictionaryDto;
 import ru.vstu.adddict.dto.dictionary.DictionaryResponseDto;
@@ -12,6 +13,8 @@ import ru.vstu.adddict.entity.dictionary.BaseDictionary;
 import ru.vstu.adddict.entity.dictionary.Dictionary;
 
 import java.time.LocalDateTime;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Mapper(
         componentModel = "spring",
@@ -56,4 +59,18 @@ public interface DictionaryMapper {
     }
 
     DictionaryResponseDto toDictionaryResponseDto(DictionaryDto dictionary);
+
+    default <S, D> PageResponseDto<D> fromPageResponseDto(PageResponseDto<S> page, Function<S, D> map) {
+        return PageResponseDto.<D>builder()
+                .page(page.getPage())
+                .pageSize(page.getPageSize())
+                .totalPages(page.getTotalPages())
+                .totalElements(page.getTotalElements())
+                .content(
+                        page.getContent().stream()
+                                .map(map)
+                                .collect(Collectors.toList())
+                )
+                .build();
+    }
 }
