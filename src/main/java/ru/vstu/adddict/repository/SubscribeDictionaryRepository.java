6a96d5702a7789c6801a86e89bc4dc6bcae1206b
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-05-19 01:27:38 +0300

    feat: added method for getting subscribe dictionaries by user id

--- src/main/java/ru/vstu/adddict/repository/SubscribeDictionaryRepository.java
+++ src/main/java/ru/vstu/adddict/repository/SubscribeDictionaryRepository.java
@@ -1,6 +1,8 @@
 package ru.vstu.adddict.repository;
 
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.vstu.adddict.entity.subscribedictionary.SubscribeDictionary;
 
@@ -10,4 +12,6 @@ public interface SubscribeDictionaryRepository extends JpaRepository<SubscribeDi
     Optional<SubscribeDictionary> findByUserIdAndDictionaryId(Long userId, Long dictionaryId);
 
     Optional<SubscribeDictionary> findByDictionaryIdAndUserId(Long dictionaryId, Long userId);
+
+    Page<SubscribeDictionary> getSubscribeDictionariesByUserId(Long userId, Pageable pageable);
 }
