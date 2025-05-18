ed1b072c3d831a5452c346fb016fbc2d177c6bbc
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-05-19 01:26:03 +0300

    feat: added page mapper in SubscribeDictionaryMapper

--- src/main/java/ru/vstu/adddict/mapper/SubscribeDictionaryMapper.java
+++ src/main/java/ru/vstu/adddict/mapper/SubscribeDictionaryMapper.java
@@ -4,12 +4,15 @@ import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
+import ru.vstu.adddict.dto.PageResponseDto;
 import ru.vstu.adddict.dto.subscribedictionary.CreateSubscribeDictionaryRequestDto;
 import ru.vstu.adddict.dto.subscribedictionary.SubscribeDictionaryDto;
 import ru.vstu.adddict.dto.subscribedictionary.SubscribeDictionaryResponseDto;
 import ru.vstu.adddict.entity.subscribedictionary.SubscribeDictionary;
 
 import java.time.LocalDateTime;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Mapper(
         componentModel = "spring",
@@ -25,4 +28,18 @@ public interface SubscribeDictionaryMapper {
     SubscribeDictionaryDto toDto(SubscribeDictionary savedSubscribeDictionary);
 
     SubscribeDictionaryResponseDto toSubscribeDictionaryResponseDto(SubscribeDictionaryDto responseDto);
+
+    default <S, D> PageResponseDto<D> fromPageResponseDto(PageResponseDto<S> page, Function<S, D> map) {
+        return PageResponseDto.<D>builder()
+                .page(page.getPage())
+                .pageSize(page.getPageSize())
+                .totalPages(page.getTotalPages())
+                .totalElements(page.getTotalElements())
+                .content(
+                        page.getContent().stream()
+                                .map(map)
+                                .collect(Collectors.toList())
+                )
+                .build();
+    }
 }
